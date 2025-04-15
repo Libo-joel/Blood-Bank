9e155235e398a07d56c936a87fc9389c28e029f2
Libo Joel <imlibo3@gmail.com>
2025-04-15 10:00:50 +0530

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,12 @@
+-- Donors
+INSERT INTO Donors VALUES (1, 'Raj', 28, 'A+', '2024-12-01');
+INSERT INTO Donors VALUES (2, 'Divya', 35, 'B-', '2025-02-15');
+INSERT INTO Donors VALUES (3, 'Kiran', 22, 'O+', '2024-11-10');
+
+-- Hospitals
+INSERT INTO Hospitals VALUES (1, 'Apollo Hospital', 'Chennai');
+INSERT INTO Hospitals VALUES (2, 'CMC Hospital', 'Vellore');
+
+-- Requests
+INSERT INTO Requests VALUES (1, 1, 'O+', 3, '2025-04-12');
+INSERT INTO Requests VALUES (2, 2, 'A+', 2, '2025-04-14');
